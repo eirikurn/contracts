c051af4ab4dd0e53d9a96cea4c41087cf06950a0
Eirikur Nilsson <eirikur@nilsson.is>
2011-05-15 21:24:05 +0000

    Tweaked tests.

--- test/contracts.test.js
+++ test/contracts.test.js
@@ -4,11 +4,20 @@ var c = require('contracts')
   , should = require('should');
 
 module.exports = {
+  'validate()': function() {
+    var report = c.validate(5, { type: 'string' });
+    report.errors.length.should.not.equal(0);
+    var report = c.validate("foo", { type: 'string' });
+    report.errors.length.should.equal(0);
+    report.instance.should.equal("foo");
+  }
+
   'transform()': function() {
-    var result = c.transform("5", { filter: f.toInt() });
-    result.should.equal(5);
+    var report = c.transform("5", { filter: f.toInt() });
+    report.should.equal(5);
     assert.throws(function() { c.transform("5", { type: "integer" }); }, c.ValidationError);
   },
+
   'wrap()': function() {
     var schema = { items: [{type: 'string'}], filter: f.cleanArray() };
     var wrapped = c.wrap(schema, function(str1, str2) {
@@ -17,6 +26,7 @@ module.exports = {
     wrapped("foo", "bar");
     assert.throws(function() { wrapped(5, 6); }, c.ValidationError);
   },
+
   'view()': function() {
     var schema = { filter: f.toInt(), type: 'integer' };
     var viewMiddleware = c.view(schema);
@@ -24,17 +34,17 @@ module.exports = {
     req = {params: '5'};
     called = false;
     viewMiddleware(req, {}, function() { called = true; });
-    called.should.be.true
+    called.should.be.true;
     req.data.should.equal(5);
 
     req = {params: 'foobar'};
-    called = false
+    called = false;
     viewMiddleware(req, {}, function(error) {
       error.should.be.an.instanceof(c.ValidationError);
       called = true;
     });
-    called.should.be.true
+    called.should.be.true;
     should.not.exist(req.data);
-  },
+  }
 };
 
