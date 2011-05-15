04295d5c4547e26e5ea3fe8b5e45e8475a61f201
Eirikur Nilsson <eirikur@nilsson.is>
2011-05-15 17:44:42 +0000

    Wrote a test for other JSV patch.

--- test/schema.test.js
+++ test/schema.test.js
@@ -8,5 +8,11 @@ module.exports = {
   'NaN is not a number': function() {
     var report = validate(NaN, {type: 'number'});
     report.errors.length.should.equal(1);
-  }
+  },
+  'Filter can transform instance before validation': function() {
+    var schema = {type: 'number', filter: function(value) { return parseInt(value, 10); }};
+    var report = validate("5", schema);
+    report.errors.length.should.equal(0);
+    report.instance.getValue().should.equal(5);
+  },
 };
